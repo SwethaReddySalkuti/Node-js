8666873e56d0bdb2007e403ef4dc579093c8d427
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-04-13 14:42:17 +0530

    Create Server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+var http = require('http');
+
+
+ http.createServer(function(){
+    
+    res.write('My Name is Swetha');
+    res.end;
+}).listen(4000)
+
+
+
+
+
+
+
+
+
